1f41f8e95a32e13575db9e5ed262000626860661
Rob Bates <tacobates@gmail.com>
2016-10-04 10:21:09 -0600

    Update RenderGame.java

--- RenderGame.java
+++ RenderGame.java
@@ -2,3 +2,6 @@
 //TODO: this class can then be added to our GUI
 //TODO: we'll make an array of them to use when paging.
   //TODO: if there aren't enough games to fill the page, this objects understands how to render itself when empty
+
+//NOTE: DON'T just make Game extend JPanel for performance reasons
+  //This way we only have a few GUI elements in memory, even though we have lots of lightweight Game objects
