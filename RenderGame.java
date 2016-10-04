5803c84fd4a88005f10a1c39fe84db1ab30dc93a
Rob Bates <tacobates@gmail.com>
2016-10-04 10:19:34 -0600

    Create RenderGame.java

---
+++ RenderGame.java
@@ -0,0 +1,4 @@
+//TODO: a class that extends a JPanel, contains a Game, and knows how to pack() itself
+//TODO: this class can then be added to our GUI
+//TODO: we'll make an array of them to use when paging.
+  //TODO: if there aren't enough games to fill the page, this objects understands how to render itself when empty
