738a2e51b29da1bc6f175589549ae781ba706750
Rob Bates <tacobates@gmail.com>
2016-10-10 15:53:22 -0600

    NES launching in FCEUX

--- GameNes.java
+++ GameNes.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.lang.reflect.Field;
+
 /**
 * For NES games to be run in an Emulator
 */
@@ -6,5 +9,35 @@ public class GameNes extends Game {
 		super();
 		setType(TYPE_NES);
 		setDir(meta.dirNes);
+	}
+
+	public long launch() {
+		long pid = -1;
+
+		String path = filePath();
+//TODO: check that fceux is installed
+		String cmd = "fceux \"" + path + "\"";
+System.out.println(cmd);
+
+		try {
+			Runtime rt = Runtime.getRuntime();
+			String[] cmdArray = {"lxterminal", "-e", cmd};
+			Process p = rt.exec(cmdArray);
+
+			if (p.getClass().getName().equals("java.lang.UNIXProcess")) {
+				Field f = p.getClass().getDeclaredField("pid");
+				f.setAccessible(true);
+				pid = f.getLong(p);
+				f.setAccessible(false);
+				System.out.println("Pid: " + Long.toString(pid));
+			}
+
+			p.waitFor();
+		} catch (Exception e) {
+			System.out.println("###ERROR: Generic###");
+			System.out.println(e);
+		}
+
+		return pid;
 	}
 }
